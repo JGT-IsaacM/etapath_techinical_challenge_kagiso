9914feb1e546424b5c426e3fe5623902c153194e
Kagiso Mogano <kaymogano@gmail.com>
2023-05-03 22:42:42 +0200

    Added funtion to get rid of stored token

--- reactapp/my-app/src/components/App/useToken.js
+++ reactapp/my-app/src/components/App/useToken.js
@@ -3,19 +3,26 @@ import { useState } from 'react';
 export default function useToken() {
 
     const getToken = () => {
-        const tokenString = sessionStorage.getItem('token');
-        const userToken = JSON.parse(tokenString);
-        return userToken?.token
-      };
+      const tokenString = sessionStorage.getItem('token');
+      const userToken = JSON.parse(tokenString);
+      return userToken?.token
+    };
     
     const [token, setToken] = useState(getToken());
     const saveToken = userToken => {
-        sessionStorage.setItem('token', JSON.stringify(userToken));
-        setToken(userToken.token);
-      };
+      sessionStorage.setItem('token', JSON.stringify(userToken));
+      setToken(userToken.token);
+    };
+
+    const destroyToken = () => {
+      sessionStorage.removeItem('token');
+      setToken(null);
+    };
+
       
     return {
     setToken: saveToken,
+    destroyToken,
     token
     }
 
