84caf19c117c10424e84ef6049f93396a84539c1
Kagiso Mogano <kaymogano@gmail.com>
2023-05-03 23:17:47 +0200

    Added path to ensure component reloads

--- reactapp/my-app/src/components/Home/Package/Package.js
+++ reactapp/my-app/src/components/Home/Package/Package.js
@@ -68,7 +68,7 @@ export default class Package extends React.Component{
               }}>
               <button>Edit</button>
             </Link>
-            <Link to = "#">
+            <Link to = "/">
               <button onClick = {this.onDelete}>Delete</button>
             </Link>
           </div>
