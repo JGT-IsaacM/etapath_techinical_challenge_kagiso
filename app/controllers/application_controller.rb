166693c81901b344b683b18b9c8c4d955a3d4ab8
Kagiso Mogano <kaymogano@gmail.com>
2023-05-03 10:51:59 +0200

    Updated the application controller to override the authenticate_user function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+require 'knock'
 class ApplicationController < ActionController::Base
+    include Knock::Authenticable
+
+    private
+
+     def authenticate_user
+        token = request.headers['Authorization']
+        authenticate_for(token)
+    end
 end
